3fdf82f61bdae9c00ce2aea22f9c3c3ce19fb5fb
Samuel Spellman <spellman@rockhopper-02.cs.wisc.edu>
2022-07-12 20:38:10 -0500

    Needed to stage directory

---
+++ ch5/pntrsArrs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* 
+ * This code will show both the differences and similarities between pointers
+ * and arrays. 
+ * It will also have some useful tips for using pointers and arrays correctly.
+ */
+int main()
+{
+	// Start code here.	
+}
