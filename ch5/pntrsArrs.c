197bf96a39bd4be8fd10a777f3cf053f012872cd
Samuel Spellman <spellman@royal-13.cs.wisc.edu>
2022-07-12 21:30:28 -0500

    Added the k&r code for pointers

--- ch5/pntrsArrs.c
+++ ch5/pntrsArrs.c
@@ -7,5 +7,19 @@
  */
 int main()
 {
-	// Start code here.	
+	// Start code here. (Source)-> Kernighan, Brian W., & Ritchie, Dennis M.
+	// (1988). The C Programming Language (Second Edition). Bell Telephone 
+	// Laboratories, Incorporated.	
+	int x = 1, y = 2, z[10];
+	int *ip;		/* ip is an int pointer */
+
+	ip = &x;		/* ip points to the address of x. */
+	y = *ip;		/* y is now 1 (ip is "dereferenced" aka value returned). */
+	*ip = 0;		/* ip is dereferenced again, so the x addr is fetched. */
+					/* This causes 0 to be stored at x's address(x=0) */
+	ip = &z[0];		/* ip is now pointed to the 0th element of z */
+					
+	// MY CODE BELOW**********************************************************
+		
+	
 }
