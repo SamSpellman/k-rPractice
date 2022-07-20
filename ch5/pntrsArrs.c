f80fae224baaa35bfb310f49dd13ff08b479d4da
Samuel Spellman <spellman@royal-28.cs.wisc.edu>
2022-07-19 21:56:03 -0500

    added personal code for ch5

--- ch5/pntrsArrs.c
+++ ch5/pntrsArrs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /* 
  * This code will show both the differences and similarities between pointers
  * and arrays. 
@@ -20,6 +21,42 @@ int main()
 	ip = &z[0];		/* ip is now pointed to the 0th element of z */
 					
 	// MY CODE BELOW**********************************************************
-		
 	
+	// I will write an array and print out different indexes to show
+	// how C stores them contiguosly. I also want to see how much storage is 
+	// used.
+	int storage[] = {12, 4, 99, 12345};
+	int i = 0;
+	// Iterate with a while loop (until char='\0')
+	while(storage[i] != '\0') {
+		printf("storage[%d] = %d \t location = %p\n", i, storage[i],
+				&storage[i]);
+		i++;
+	}
+	// This will print extra characters after the array, because the length
+	// of the array isn't explicitly stated.
+	printf("\nNow the correct way\n");
+	// I will specify the size in the declaration, and also hard code the 
+	// null termination char in two arrays.
+	int specSize[5] = {7, 83, 6, 82, 1111};
+	int specEndChar[] = {3, 56, 9393, '\0'};
+	i = 0;
+
+	printf("\nfirst, with a size declaration, specSize[5]:\n");
+	// testing explicit size implementation
+	while(specSize[i] != '\0') {
+		printf("specSize[%d] = %d \t location = %p\n", i, 
+				specSize[i], &specSize[i]);
+		i++;
+	}
+
+	printf("\nThen, with an end char specification, {..., \\0}:\n");
+	i = 0;
+	// testing array with explicit null terminating char
+	while(specEndChar[i] != '\0') {
+		printf("specEndChar[%d] = %d \t location = %p\n", i, 
+				specEndChar[i], &specEndChar[i]);
+		i++;
+	}
+
 }
